cc534fa3c52ed2cce18be5a930bde5893c6c7c15
João Luís Canelhas Felício <44256600+Joaolfelicio@users.noreply.github.com>
2019-12-21 09:31:47 +0100

    Init

---
+++ ChessGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
